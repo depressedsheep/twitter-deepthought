7f7e15d0ecc57dc4d92fcdefb56f996b27a67a7d
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2015-03-12 19:30:07 +0800

    compress and decompress working, streams in english, started boto

--- crawler.py
+++ crawler.py
@@ -2,12 +2,18 @@ import os, sys, time, datetime
 import twitter
 import json
 from config import ck, cs, ot, ots
+from boto.s3.connection import S3Connection
+try:
+	from config import boto_access, boto_secret
+except:
+	pass
 #import io
 import bz2 #file compression
 
 RAW_FILE = 'raw'
 RAW_COMPRESSED_FILE = 'raw_compressed'
-
+DECOMPRESSED_FILE = 'raw_decompressed'
+BUCKET_NAME = 'twitter-deepthought'
 def oauth_login(): #authenticate w twitter API
 	CONSUMER_KEY = ck
 	CONSUMER_SECRET = cs
@@ -24,27 +30,33 @@ def crawl():
 	twitter_api = oauth_login()
 	print twitter_api
 	twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
-	stream = twitter_stream.statuses.sample()
-	c = 2 #placeholder, just to make sure the streaming ends for debugging
+	stream = twitter_stream.statuses.sample(language = 'en')
+	#c = 2 #placeholder, just to make sure the streaming ends for debugging
 	outfile = open(RAW_FILE,'w') #file is opened for its lifetime as it is a better practice
 	outcompress = bz2.BZ2Compressor()
 	datetime_ = str(datetime.datetime.now())[:-7]
 	outcompressfile = open('raw_compressed','w')
 	for tweet in stream:
-		c -= 1
+		#c -= 1
 		current_time = int(time.time()) - start_time
 		current_datetime = str(datetime.datetime.now())[:-7]
 		outcompress.compress(json.dumps(tweet) + '\n\n')
 
 		#print json.dumps(tweet, indent = 1)
+		print current_time
+		
 		json.dump(tweet,outfile)
+		try: 
+			tweet['text'].encode('ascii', 'ignore')
+		except:
+			pass
 		outfile.write('\n')
-		if c == 0:
+		if current_time > 2:
 			outcompressfile.write(outcompress.flush())
-			#print len(repr(outcompress.flush()))
+			#print len(repr(outcompress.flush()))2
 			break
 def read():
-	f = open(RAW_FILE ,'r')
+	f = open(DECOMPRESSED_FILE ,'w')
 	#for line in f:
 	#	print line
 	
@@ -56,8 +68,13 @@ def read():
 
 		if '\n' in _text:
 			print "!!!"
-		print(len(_text))
+			f.write(str(_text.split('\n')))
 
-if __name__ == '__main__':
-	#crawl()
-	read()
+def boto_save():
+	conn = S3Connection(boto_access, boto_secret)
+def boto_init():
+	conn = S3Connection(boto_access, boto_secret)
+	bucket = conn.create_bucket(BUCKET_NAME)
+if __name__ == "__main__":
+	crawl()	
+	#read()
