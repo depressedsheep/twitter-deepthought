790e25e4c6b978c6a418d1027ec8556f097b4425
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2015-03-22 17:46:08 +0800

    load from boto

--- botostuff.py
+++ botostuff.py
@@ -5,7 +5,7 @@ try:
 except:
 	pass
 
-def boto_save(key, filename, BUCKET_NAME = 'twitter-deepthought'):
+def save(key, filename, BUCKET_NAME = 'twitter-deepthought'):
 	conn = S3Connection(boto_access, boto_secret)
 	try:
 		bucket = conn.create_bucket(BUCKET_NAME, location =Location.SAEast)
@@ -14,3 +14,11 @@ def boto_save(key, filename, BUCKET_NAME = 'twitter-deepthought'):
 	k = Key(bucket)
 	k.key = key
 	k.set_contents_from_filename(filename)
+def load(key, BUCKET_NAME = 'twitter-deepthought'):
+	conn = S3Connection(boto_access, boto_secret)
+	bucket = conn.get_bucket(BUCKET_NAME)
+	k = Key(bucket)
+	k.key = key
+	print bucket.list()
+if __name__ == '__main__':
+	print "ASDF"
